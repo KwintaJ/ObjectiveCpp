e1e198432c7a39b1b51cf6fb6c5537ba6c4f7efd
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-03-26 14:13:58 +0100

    Update Zdanie4.cpp

--- Zestaw4/Zdanie4.cpp
+++ Zestaw4/Zdanie4.cpp
@@ -60,8 +60,8 @@ struct char_sum
 template <typename Iter>
 typename sum_traits<typename std::iterator_traits<Iter>::value_type>::sum_type sum(Iter begin, Iter end) 
 {
-    using sum_type = typename sum_traits<typename std::iterator_traits<Iter>::value_type>::sum_type;
-    sum_type total = ::zero(); 
+    typedef typename sum_traits<typename std::iterator_traits<Iter>::value_type>::sum_type sum_type;
+    sum_type total = sum_traits<typename std::iterator_traits<Iter>::value_type>::zero(); 
     for(auto it = begin; it != end; ++it)
     {
         total += *it;
