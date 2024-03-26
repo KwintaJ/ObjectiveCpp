7ca5e77a17aaa165e001603a9f2286cba9dadbcd
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-03-26 12:32:38 +0100

    Update Zadanie1.cpp

--- Zestaw4/Zadanie1.cpp
+++ Zestaw4/Zadanie1.cpp
@@ -1,1 +1,16 @@
-//
+//######################################//
+//                                      //
+//    Jan Kwinta          26.03.2024    //
+//                                      //
+//    Obiektowy C++                     //
+//    Zestaw 4, zadanie 1               //
+//                                      //
+//######################################//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+}
