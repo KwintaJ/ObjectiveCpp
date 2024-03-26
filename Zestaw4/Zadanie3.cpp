7403a0125d86ec9027a25a7f1c6f95b9d2ae6b25
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-03-26 13:24:02 +0100

    Update Zadanie3.cpp

--- Zestaw4/Zadanie3.cpp
+++ Zestaw4/Zadanie3.cpp
@@ -48,18 +48,32 @@ struct sum_traits<double>
         return 0.0;
     }
 };
-
-template<typename T> 
-typename sum_traits<T>::sum_type sum(T *beg,T *end)
+struct char_sum
 {
-    typedef typename sum_traits<T>::sum_type sum_type;
-    sum_type total = sum_traits<T>::zero(); 
-    while(beg != end)
+    typedef char sum_type;
+    static sum_type zero()
+    {
+        return (char)0;
+    }
+};
+
+template<typename Traits, typename T>
+typename Traits::sum_type sum(T *beg, .T *end)
+{
+    typedef typename Traits::sum_type sum_type;
+    sum_type total = sum_type::zero(); 
+    while(beg != end )
     { 
         total += *beg;
         beg++; 
-    }
+    } 
     return total; 
+};
+
+template<typename T> 
+typename sum_traits<T>::sum_type sum(T *beg, T *end)
+{ 
+    return sum<sum_traits<T>, T>(beg, end);
 }
 
 int main(int argc, char **argv)
@@ -71,7 +85,8 @@ int main(int argc, char **argv)
     std::cout << "Suma int: " << sum(ints, ints + 5) << std::endl;
 
     char chars[] = {'a', 'b', 'c'};
-    std::cout << "Suma char: " << sum(chars, chars + 3) << std::endl;
+    std::cout << "Suma char1: " << sum(chars, chars + 3) << std::endl;
+    std::cout << "Suma char2: " << sum<char_sum>(chars, chars + 3) << std::endl;
 
     float floats[] = {1.1, 2.2, 3.3};
     std::cout << "Suma float: " << sum(floats, floats + 3) << std::endl;
