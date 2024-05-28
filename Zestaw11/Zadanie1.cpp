b140ad885517d0f24f9197ca1e85805d357853c0
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-05-28 16:22:02 +0200

    Update Zadanie1.cpp

--- Zestaw11/Zadanie1.cpp
+++ Zestaw11/Zadanie1.cpp
@@ -7,8 +7,33 @@
 //                                      //
 //######################################//
 
+#include <concepts>
 #include <iostream>
 
-int main()
-{
+// Funkcja szablonowa gcd, która działa tylko dla typów całkowitoliczbowych
+template<std::integral T>
+T gcd(T a, T b) {
+    while (b != 0) {
+        T temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a = 56;
+    int b = 98;
+    std::cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << std::endl;
+
+    long c = 56L;
+    long d = 98L;
+    std::cout << "GCD of " << c << " and " << d << " is " << gcd(c, d) << std::endl;
+
+    // Kompilator zwróci błąd, jeśli użyjemy typów niecałkowitoliczbowych:
+    // double e = 56.0;
+    // double f = 98.0;
+    // std::cout << "GCD of " << e << " and " << f << " is " << gcd(e, f) << std::endl;
+
+    return 0;
 }
