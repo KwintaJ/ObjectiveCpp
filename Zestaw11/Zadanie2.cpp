0f77d59fa6e64aa05fa4c83a5b1bf50a02527756
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-05-28 16:32:31 +0200

    Update Zadanie2.cpp

--- Zestaw11/Zadanie2.cpp
+++ Zestaw11/Zadanie2.cpp
@@ -3,6 +3,36 @@
 //    Jan Kwinta          28.05.2024    //
 //                                      //
 //    Obiektowy C++                     //
-//    Zestaw 11, zadanie 1              //
+//    Zestaw 11, zadanie 2              //
 //                                      //
 //######################################//
+
+#include <iostream>
+#include <compare>
+#include <concepts>
+
+class MyClass {
+public:
+    int value;
+    auto operator<=>(const MyClass&) const = default;
+};
+
+template <std::three_way_comparable T>
+void compare_objects(const T& a, const T& b) {
+    if (a < b) {
+        std::cout << "a is less than b\n";
+    } else if (a > b) {
+        std::cout << "a is greater than b\n";
+    } else {
+        std::cout << "a is equal to b\n";
+    }
+}
+
+int main() {
+    MyClass obj1{10};
+    MyClass obj2{20};
+
+    compare_objects(obj1, obj2);
+
+    return 0;
+}
